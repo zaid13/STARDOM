71d6a1bf92a8f7e53c206be1606aa72c1085a489
Hamza Hassan <hassanhamza930@gmail.com>
2022-05-01 12:44:01 +0500

    Removed Unnecesserancy Featrures like Profile Fields Edit.

--- lib/pages/user/home/profilePage/profilePage.dart
+++ lib/pages/user/home/profilePage/profilePage.dart
@@ -10,14 +10,7 @@ import '../../../../providers/AuthProvider.dart';
 import 'package:image_picker/image_picker.dart';
 
 UserModel currentUser =
-    UserModel(email_address: "", phone_number: "", picture_url: "", uid: "",user_name: "",balance: 0.0);
-
-var testingData = [
-  1,
-  2,
-  3,
-  4,
-];
+    UserModel(email_address: "", phone_number: "", picture_url: "", uid: "", user_name: "", balance: 0.0);
 
 class profilePage extends StatefulWidget {
   const profilePage({Key? key}) : super(key: key);
@@ -27,27 +20,21 @@ class profilePage extends StatefulWidget {
 }
 
 class _profilePageState extends State<profilePage> {
+  TextEditingController phone = TextEditingController();
 
-
-  TextEditingController phooneCtrl = TextEditingController();
-
-  String imgUrl = '';
+  String imgUrl = "https://pbs.twimg.com/profile_images/1514306828913758214/mlKNRfdI_400x400.jpg";
   String pickedImage = '';
-
   bool isEditmode = false;
 
   @override
   void initState() {
     fetch() async {
-        UserModel user = await context.read<AuthProvider>().user.first;
+      UserModel user = await context.read<AuthProvider>().user.first;
       setState(() {
-        currentUser=user;
-
-        phooneCtrl.text= user.phone_number;
-        imgUrl= user.picture_url;
-
+        currentUser = user;
+        phone.text = user.phone_number;
+        imgUrl = user.picture_url;
       });
-
     }
 
     fetch();
@@ -73,8 +60,7 @@ class _profilePageState extends State<profilePage> {
                     margin: EdgeInsets.all(15),
                     child: Text(
                       "Profile",
-                      style: sofiaStyle(
-                          color: Colors.white, fontSize: 36, bold: true),
+                      style: sofiaStyle(color: Colors.white, fontSize: 36, bold: true),
                     )),
                 Container(
                   margin: EdgeInsets.only(top: 10),
@@ -96,150 +82,55 @@ class _profilePageState extends State<profilePage> {
                             children: [
                               Align(
                                 alignment: Alignment.centerLeft,
-                                child: GestureDetector(
-                                  onTap: () async {
-
-                                    if(isEditmode){
-                                      final ImagePicker _picker   = ImagePicker();
-                                      XFile? xfile =await _picker.pickImage(source: ImageSource.gallery);
-
-
-                                      setState(() {
-                                        pickedImage =xfile!.path==null?'':xfile.path;
-                                      });
-                                    }
-
-
-                                  },
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.red,
-                                        borderRadius:
-                                            BorderRadius.all(Radius.circular(10)),
-                                        image: DecorationImage(
-                                            image: pickedImage !=''?FileImage(File(pickedImage)):imgUrl!=''?
-                                            NetworkImage(imgUrl) as ImageProvider :
-                                            AssetImage("images/backBlurred.jpg"),
-                                            fit: BoxFit.cover)),
-                                    width: width * 0.5,
-                                    height: 200,
-                                  ),
-                                ),
-                              ),
-
-                              Expanded(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-                                  children: [
-
-                                    Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Container(
-                                            margin: EdgeInsets.only(top: 10),
-                                            padding: EdgeInsets.all(10),
-                                            child: Row(
-                                              mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  "Balance \nPkr ${ context.read<AuthProvider>().currentUser.balance}",
-                                                  style: sofiaStyle(
-                                                      color: Colors.white,
-                                                      fontSize: 24,
-                                                      bold: true),
-                                                ),
-                                              ],
-                                            ))),
-                                    Container(),
-                                  ],
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                                      image: DecorationImage(
+                                          image: NetworkImage(imgUrl) as ImageProvider, fit: BoxFit.cover)),
+                                  width: width * 0.5,
+                                  height: 200,
                                 ),
                               ),
                             ],
                           ),
-
-
-
                           Align(
-                              alignment: Alignment.centerLeft,
-                              child: Container(
-                                  padding: EdgeInsets.only(top: 15),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "${currentUser.user_name}",
-                                        style: sofiaStyle(
-                                            color: Colors.white,
-                                            fontSize: 35,
-                                            bold: true),
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      !isEditmode?  GestureDetector(
-                                        onTap: (){
-                                          setState(() {
-                                            isEditmode =!isEditmode;
-                                          });
-                                        },
-                                        child: Icon(
-                                          Icons.edit,
-                                          color: Colors.white,
-                                        ),
-                                      ):GestureDetector(
-                                        onTap: () async {
-                                        UserModel tempUser=  context.read<AuthProvider>().currentUser;
-                                  String tempUrl = '';
-
-                                        if(pickedImage!=''){
-                                          tempUrl =await  context.read<AuthProvider>().  uploadImageToFirebaseGetUrl(pickedImage);
-                                        }
-
-                                          context.read<AuthProvider>().  updateUser(UserModel(
-                                              picture_url: tempUrl!=''?tempUrl
-                                                  : tempUser.user_name ,
-                                              phone_number: phooneCtrl.text,
-                                              user_name: tempUser.user_name,
-                                              balance: tempUser.balance,
-                                              email_address:tempUser.email_address,
-                                              uid:tempUser.uid
-                                          ));
-
-                                          setState(() {
-                                            isEditmode =!isEditmode;
-                                          });
-                                        },
-                                        child: Icon(       Icons.save,
-                                          color: Colors.green,),
-                                      )
-                                    ],
-                                  ))),
+                            alignment: Alignment.centerLeft,
+                            child: Container(
+                              padding: EdgeInsets.only(top: 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${currentUser.user_name}",
+                                    style: sofiaStyle(color: Colors.white, fontSize: 35, bold: true),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                           Align(
                               alignment: Alignment.centerLeft,
                               child: Container(
                                   margin: EdgeInsets.only(top: 20),
                                   padding: EdgeInsets.all(10),
                                   child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
                                     children: [
                                       Text(
                                         "Email",
-                                        style: sofiaStyle(
-                                            color: Colors.white,
-                                            fontSize: 18,
-                                            bold: true),
+                                        style: sofiaStyle(color: Colors.white, fontSize: 18, bold: true),
                                       ),
                                     ],
                                   ))),
                           Container(
                             width: width,
                             child: TextField(
-                              readOnly: !isEditmode ,
+                              readOnly: true,
                               enabled: false,
-
                               style: TextStyle(
                                   fontSize: 15,
                                   color: Colors.black.withOpacity(0.8),
@@ -264,24 +155,20 @@ class _profilePageState extends State<profilePage> {
                                   margin: EdgeInsets.only(top: 10),
                                   padding: EdgeInsets.all(10),
                                   child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
                                     children: [
                                       Text(
                                         "Phone",
-                                        style: sofiaStyle(
-                                            color: Colors.white,
-                                            fontSize: 18,
-                                            bold: true),
+                                        style: sofiaStyle(color: Colors.white, fontSize: 18, bold: true),
                                       ),
                                     ],
                                   ))),
                           Container(
                             width: width,
                             child: TextField(
-                              readOnly: !isEditmode ,
-                              keyboardType:TextInputType.phone,
- controller: phooneCtrl,
+                              readOnly: true,
+                              keyboardType: TextInputType.phone,
+                              controller: phone,
                               style: TextStyle(
                                   fontSize: 15,
                                   color: Colors.black.withOpacity(0.8),
@@ -300,8 +187,6 @@ class _profilePageState extends State<profilePage> {
                               ),
                             ),
                           ),
-
-
                           SizedBox(
                             height: 30,
                           ),
@@ -313,21 +198,15 @@ class _profilePageState extends State<profilePage> {
                                 alignment: Alignment.centerLeft,
                                 child: Container(
                                     decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(10)),
-                                        color: Colors.red),
+                                        borderRadius: BorderRadius.all(Radius.circular(10)), color: Colors.red),
                                     margin: EdgeInsets.only(top: 10),
                                     padding: EdgeInsets.all(10),
                                     child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.center,
                                       children: [
                                         Text(
                                           "Log Out",
-                                          style: sofiaStyle(
-                                              color: Colors.white,
-                                              fontSize: 18,
-                                              bold: true),
+                                          style: sofiaStyle(color: Colors.white, fontSize: 18, bold: true),
                                         ),
                                       ],
                                     ))),
